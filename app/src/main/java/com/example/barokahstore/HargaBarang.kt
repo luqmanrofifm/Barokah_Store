fa926fe07d8aa6c2fe149ee85f37481b97f31145
luqmanrofifm <luqmanrmrofif@yahoo.co.id>
2021-01-07 13:16:54 +0700

    Initial commit

---
+++ app/src/main/java/com/example/barokahstore/HargaBarang.kt
@@ -0,0 +1,8 @@
+package com.example.barokahstore
+
+data class HargaBarang(
+    var judul: String = "",
+    var harga: String = "",
+    var keterangan: String = ""
+
+)
---
+++ app/src/main/java/com/example/barokahstore/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.barokahstore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var rvDaftarHarga : RecyclerView
+    private var list = ArrayList<HargaBarang>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        rvDaftarHarga = findViewById(R.id.rv_daftar_harga)
+        rvDaftarHarga.setHasFixedSize(true)
+
+        list.addAll(DataHarga.listData)
+        showRecyclerList()
+    }
+
+    private fun showRecyclerList() {
+        rvDaftarHarga.layoutManager = LinearLayoutManager(this)
+        val listDaftarHargaAdapter = DaftarHargaAdapter(list)
+        rvDaftarHarga.adapter = listDaftarHargaAdapter
+
+    }
+
+
+}
